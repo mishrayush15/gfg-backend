858ed91ca926713399a64b0ca471faab10add405
mishrayush15 <ayushmishra780193@gmail.com>
2025-01-30 17:20:03 +0530

    initialize MONGO DB

--- main.js
+++ main.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 const testRoute = require('./routes/gfg-test-router')
+const mongoose = require('mongoose');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+mongoose.connect(MONGO_URI)
+    .then(()=>{
+        console.log("Connected to database!");
+    })
+    .catch(()=>{
+        console.error("Unable to connect to the database!");
+    });
 
 app.use('/api/v1', testRoute)
 
