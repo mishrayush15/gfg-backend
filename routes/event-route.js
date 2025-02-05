6f0726cf88b64b4afd05c7e079f780122c277fc4
mishrayush15 <ayushmishra780193@gmail.com>
2025-02-05 17:34:58 +0530

    add image-upload

--- routes/event-route.js
+++ routes/event-route.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const createEvent = require('../controllers/create-event')
-const {GetAllEvents, GetEventById} = require('../controllers/fetch-event')
+const { GetAllEvents, GetEventById } = require('../controllers/fetch-event')
 const DeleteEvent = require('../controllers/delete-event')
 const UpdateEvent = require('../controllers/update-event');
+const multer = require('multer')
 
-router.post('/event/create', createEvent)
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './posters')
+    },
+    filename: function (req, file, cb) {
+        
+        cb(null, Date.now() + 'poster' + Math.round(Math.random() * 1E9) + file.originalname)
+    }
+})
+
+const upload = multer({ storage });
+
+router.post('/event/create', upload.single('poster'), createEvent)
 router.get('/event/fetch', GetAllEvents)
 router.get('/event/:id', GetEventById)
 router.delete('/event/delete/:id', DeleteEvent)
